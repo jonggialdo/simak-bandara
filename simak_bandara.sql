2b131ce38b77826c4e8697d905fb19d6f20e103a
rizkysbgj <rizkysubagja99@gmail.com>
2018-11-12 20:14:26 +0700

    add vwkoderekening & vwtransaksi

--- simak_bandara.sql
+++ simak_bandara.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 12, 2018 at 12:59 PM
--- Server version: 10.1.26-MariaDB
--- PHP Version: 7.1.8
+-- Generation Time: Nov 12, 2018 at 02:11 PM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -67,6 +67,53 @@ CREATE TABLE `user` (
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `vwkoderekening`
+-- (See below for the actual view)
+--
+CREATE TABLE `vwkoderekening` (
+`kode_rekening` int(11)
+,`nama_kode` int(11)
+,`status` enum('debet','kredit','','')
+);
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `vwtransaksi`
+-- (See below for the actual view)
+--
+CREATE TABLE `vwtransaksi` (
+`kd_debet` int(11)
+,`nama_kode_debet` int(11)
+,`kd_kredit` int(11)
+,`nama_kode_kredit` int(11)
+,`tgl_transaksi` datetime
+,`Uraian` text
+,`nominal_debet` int(11)
+,`nominal_kredit` int(11)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `vwkoderekening`
+--
+DROP TABLE IF EXISTS `vwkoderekening`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vwkoderekening`  AS  select `kode_rekening`.`kode_rekening` AS `kode_rekening`,`kode_rekening`.`nama_kode` AS `nama_kode`,`kode_rekening`.`status` AS `status` from `kode_rekening` ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `vwtransaksi`
+--
+DROP TABLE IF EXISTS `vwtransaksi`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vwtransaksi`  AS  select `a`.`kd_debet` AS `kd_debet`,`b`.`nama_kode` AS `nama_kode_debet`,`a`.`kd_kredit` AS `kd_kredit`,`c`.`nama_kode` AS `nama_kode_kredit`,`a`.`tgl_transaksi` AS `tgl_transaksi`,`a`.`uraian` AS `Uraian`,`a`.`nominal_debet` AS `nominal_debet`,`a`.`nominal_kredit` AS `nominal_kredit` from ((`transaksi` `a` left join `kode_rekening` `b` on((`a`.`kd_debet` = `b`.`kode_rekening`))) left join `kode_rekening` `c` on((`a`.`kd_kredit` = `c`.`kode_rekening`))) ;
+
 --
 -- Indexes for dumped tables
 --
@@ -98,16 +145,19 @@ ALTER TABLE `user`
 --
 ALTER TABLE `kode_rekening`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
