0895da834b1ef9c446a921696908eabef6975a92
rizkysbgj <rizkysubagja99@gmail.com>
2018-12-19 22:09:35 +0700

    add authentication to controller

--- application/controllers/laporanpdf.php
+++ application/controllers/laporanpdf.php
@@ -2,6 +2,9 @@
     class Laporanpdf extends CI_Controller {
         function __construct() {
             parent::__construct();
+            if($this->session->userdata('status') != 'login') {
+                redirect(Login);
+            }
             $this->load->library('pdf');
         }
 
